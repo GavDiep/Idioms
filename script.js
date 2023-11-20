4424893cf43bb87b473eb266ba12296a9e0482e6
Sealio <146213110+GavDiep@users.noreply.github.com>
2023-11-20 10:12:39 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// Event listener things
+document.getElementById("btn1").addEventListener("click", function1);
+document.getElementById("btn2").addEventListener("click", function2);
+document.getElementById("btn3").addEventListener("click", function3);
+document.getElementById("btn4").addEventListener("click", function4);
+document.getElementById("btn5").addEventListener("click", function5);
+
+function function1() {
+  alert("the hook.");
+}
+
+function function2() {
+  alert("drawing board.");
+}
+
+function function3() {
+  alert("devil.");
+}
+
+function function4() {
+  alert("last straw.");
+}
+
+function function5() {
+  alert("around something.");
+}
